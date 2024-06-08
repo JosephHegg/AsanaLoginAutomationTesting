7b8394d236d77e91ddbacbd60d2c82c8ec93d43f
Joseph Hegg <heggjv@rose-hulman.edu>
2024-06-08 17:45:13 -0400

    Adding initial playwright config and text reference data.

---
+++ utils/loginCredentials.js
@@ -0,0 +1,3 @@
+export const ASANA_LOGIN_URL = "https://app.asana.com/-/login";
+export const ASANA_USER_EMAIL = "ben+pose@workwithloop.com";
+export const ASANA_USER_PASSWORD = "Password123";
---
+++ utils/tasksAndColumnTextValues.js
@@ -0,0 +1,14 @@
+export const TO_DO_COLUMN = "To do";
+export const NEW_REQUEST_COLUMN = "New Requests";
+export const IN_PROGRESS_COLUMN = "In Progress";
+export const COMPLETED_COLUMN = "Completed";
+
+export const CROSS_FUNCTIONAL_PROJECT = "Cross-functional project plan, Project";
+export const WORK_REQUESTS = "Work Requests";
+
+export const DRAFT_PROJECT_TASK = "Draft project brief";
+export const SCHEDULE_KICKOFF_TASK = "Schedule kickoff meeting";
+export const SHARE_TIMELINE_TASK = "Share timeline with teammates";
+export const LAPTOP_SETUP_TASK = "[Example] Laptop setup for new hire";
+export const PASSWORD_NOT_WORKING_TASK = "[Example] Password not working";
+export const NEW_KEYCARD_DANIELA_TASK = "[Example] New keycard for Daniela V";
