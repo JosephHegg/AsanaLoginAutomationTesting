7a436c140a606e88a15d6d2c6564715cf929194a
Joseph Hegg <heggjv@rose-hulman.edu>
2024-06-09 05:32:14 -0400

    Added test text databank and testCases JSON object.

---
+++ tests/loginVerificationTests.js
@@ -0,0 +1,6 @@
+const { test, expect } = require('@playwright/test');
+
+import testCases from '../utils/testDataBank';
+import * as loginCredentials from '../utils/loginCredentials';
+
+
--- utils/fieldValidationTextValues.js
+++ utils/fieldValidationTextValues.js
@@ -1,3 +1,25 @@
+export const NON_PRIORITY_TAG = "Non-Priority";
+export const LOW_PRIORITY_TAG = "Low priority";
+export const MEDIUM_PRIORITY_TAG = "Medium priority";
+export const HIGH_PRIORITY_TAG = "High Priority";
+export const AT_RISK_TAG = "At risk";
+
+export const LOW_TAG = "Low";
+export const MEDIUM_TAG = "Medium";
+export const HIGH_TAG = "High";
+
+export const ON_TRACK_TAG = "On track";
+export const OFF_TRACK_TAG = "Off track";
+export const NOT_STARTED_TAG = "Not Started";
+export const WAITING_TAG = "Waiting";
+export const DONE_TAG = "Done";
+
+export const LOW_EFFORT_TAG = "Low effort";
+
+export const NEW_HARDWARE_TAG = "New hardware";
+
+export const PASSWORD_RESET_TAG = "Password reset";
+
 export const TO_DO_COLUMN = "To do";
 export const NEW_REQUEST_COLUMN = "New Requests";
 export const IN_PROGRESS_COLUMN = "In Progress";
